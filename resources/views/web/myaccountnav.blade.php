40f5663dd65372cdc65c10e51a91a18bdf338e39
vinceclm <vincecelemin@gmail.com>
2021-06-14 01:11:43 +0800

    templated account menus

---
+++ resources/views/web/myaccountnav.blade.php
@@ -0,0 +1,32 @@
+<ul class="list-group">
+    <li class="list-group-item">
+        <a class="nav-link" href="{{ URL::to('/profile')}}">
+            <i class="fas fa-user"></i>
+            @lang('website.Profile')
+        </a>
+    </li>
+    <li class="list-group-item">
+        <a class="nav-link" href="{{ URL::to('/wishlist')}}">
+            <i class="fas fa-heart"></i>
+            @lang('website.Wishlist')
+        </a>
+    </li>
+    <li class="list-group-item">
+        <a class="nav-link" href="{{ URL::to('/orders')}}">
+            <i class="fas fa-shopping-cart"></i>
+            @lang('website.Orders')
+        </a>
+    </li>
+    <li class="list-group-item">
+        <a class="nav-link" href="{{ URL::to('/shipping-address')}}">
+            <i class="fas fa-map-marker-alt"></i>
+            @lang('website.Shipping Address')
+        </a>
+    </li>
+    <li class="list-group-item">
+        <a class="nav-link" href="{{ URL::to('/logout')}}">
+            <i class="fas fa-power-off"></i>
+            @lang('website.Logout')
+        </a>
+    </li>
+</ul>
